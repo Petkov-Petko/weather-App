386ce75e36d5a4a668d60d453f242cef7e3c9489
gushteraa <petkozlatilov1234@gmail.com>
2023-09-25 17:32:15 +0300

    added a style for mobiles

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const apiKey = "2c446d6021375b8c6df0cdb66882f9c6";
+const apiKey = "6fbaed11a9b864d48f1ec9ab8f07dc8d";
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
@@ -6,36 +6,35 @@ const searchBtn = document.querySelector(".search button");
 async function checkWeather(city) {
   const respone = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-  if (respone.status === 404){
+  if (respone.status === 404) {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
-  }else{
+  } else {
     var data = await respone.json();
 
-  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
-  document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-  let condition = data.weather[0].main  
-  const imageElement = document.querySelector(".weather-icon");
-  if (condition === "Drizzle"){
-    imageElement.src = "images/drizzle.png"
-  }else if (condition === "Rain"){
-    imageElement.src = "images/rain.png"
-  }else if (condition === "Snow"){
-    imageElement.src = "images/snow.png"
-  }else if (condition === "Mist"){
-    imageElement.src = "images/mist.png"
-  }else if (condition === "Clear"){
-    imageElement.src = "images/sun.png"
-  }else if (condition === "Clouds"){
-    imageElement.src = "images/clouds.png"
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°c";
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    let condition = data.weather[0].main;
+    const imageElement = document.querySelector(".weather-icon");
+    if (condition === "Drizzle") {
+      imageElement.src = "images/drizzle.png";
+    } else if (condition === "Rain") {
+      imageElement.src = "images/rain.png";
+    } else if (condition === "Snow") {
+      imageElement.src = "images/snow.png";
+    } else if (condition === "Mist") {
+      imageElement.src = "images/mist.png";
+    } else if (condition === "Clear") {
+      imageElement.src = "images/sun.png";
+    } else if (condition === "Clouds") {
+      imageElement.src = "images/clouds.png";
+    }
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
   }
-  document.querySelector(".weather").style.display = "block"
-  document.querySelector(".error").style.display = "none";
-  
-  }
-  
 }
 
 searchBtn.addEventListener("click", () => {
